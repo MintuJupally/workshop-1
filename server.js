b7afa2da79b2f41aa53b6d8a2ec5be37239394ac
Mintu Jupally <mintu.jupally@gmail.com>
2021-10-31 13:18:30 +0530

    Added nodemon to package.json

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
 const app = require("./app");
 
 console.log("Starting app..");
 
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
